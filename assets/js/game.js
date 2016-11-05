2e4660a2a39aa6afeaf3e8fddd238dcb93eaf051
Saleema Ali <sali1390@gmail.com>
2016-11-05 16:01:39 -0400

    Got buttons working in js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,45 @@
 $(document).ready(function(){
-    var animals = ["Animal1", "Animal2", "Animal3"];
+    var animals = ["cats", "dogs", "turtles"];
     
-    for (var i = 0; i < animals.length; i++) {
-        var newButton = $('<button>');
-        newButton.addClass('animButton');
-        newButton.attr('data-name', animals[i]);
-        newButton.text(animals[i]);
-        $("#buttons").append(newButton);
-        
-        console.log(animals[i]);
-    };
+    function generateButtons() {
+        for (var i = 0; i < animals.length; i++) {
+            //Create a new button
+            var newButton = $('<button>');
+            //Adds new class
+            newButton.addClass('animButton');
+            //Adds custom attribute
+            newButton.attr('data-name', animals[i]);
+            //Adds the item in the array as the text
+            newButton.text(animals[i]);
+            //Appends the button within the buttons div
+            $("#buttons").append(newButton);
+            //Assign the click event
+            newButton.on('click', showGifs);
+
+            console.log(animals[i]);
+        };
+    }
     
-    $(document).on('click', '#buttons', function(){
+    //function call to generate buttons
+    generateButtons();
+    
+    //Function to carry out the Ajax call and show the gifs
+    function showGifs() {
         var animalName = $(this).attr('data-name');
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC";
-//        $.ajax(url: queryURL, method: 'GET'){
-//            
-//        }
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=2";
+        $.ajax({url: queryURL, method: 'GET'}).done(function(response){
+            for (var i = 0; i < 9; i++) {
+                var rating = response.data.rating;
+                var imageUrl = response.data.images.fixed_height.url;
+                
+                console.log(rating);
+                console.log(imageUrl);
+            };
+            
+            console.log(response);
+        });
         
         console.log(animalName);
         console.log(queryURL);
-    });
+    };
 });
